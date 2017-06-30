4f97f8adee2d7bdc4fb61a720af35aa4c6f72cbf
raidersk8 <romangalchenkov@gmail.com>
2017-06-30 09:56:44 +0300

    update

--- blocks/front-page/first-screen.php
+++
@@ -1,9 +0,0 @@
-<div class="front-page-first-screen">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-1 text-right">
-				<a class="main-logo-light big-logo" href="#"></a>
-			</div>
-		</div>
-	</div>
-</div>
