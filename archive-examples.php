bf827ce54b6b24f40035faec56e0207a2887bb35
raidersk8 <romangalchenkov@gmail.com>
2017-07-05 10:11:05 +0300

    update

--- archive-examples.php
+++ archive-examples.php
@@ -1,5 +1,68 @@
 <?php get_header(); ?>	
-	<div class="page archive-examples">
-		test
+	<div class="page">
+		<div class="archive-examples">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-8 col-xs-offset-1 list">
+						<div class="row">
+							<div class="col-item col-xs-4-5">
+								<a href="#" class="item type-1" style="margin-top: 260px; background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/1.png);">
+								</a>
+							</div>
+							<div class="col-item col-xs-4-5">
+								<a href="#" class="item type-1" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/2.png);">
+								</a>						
+							</div>
+							<div class="col-item col-xs-3">
+								<a href="#" class="item type-2" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/3.png);">
+								</a>		
+								<a href="#" class="item type-2" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/4.png);">
+								</a>						
+							</div>
+							<div class="col-item col-xs-3">
+								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/5.png);">
+								</a>						
+							</div>
+							<div class="col-item col-xs-4-5">
+								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/6.png);">
+								</a>						
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-item col-xs-3">
+								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/7.png);">
+								</a>		
+								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/8.png);">
+								</a>							
+							</div>
+							<div class="col-item col-xs-6">
+								<a href="#" class="item type-4" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/9.png);">
+								</a>							
+							</div>
+							<div class="col-item col-xs-3">
+								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/10.png);">
+								</a>		
+								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/100.png);">
+								</a>						
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="top-part">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-3 col-xs-offset-1">
+							<div class="h1 main-title">Образцы продукции</div>
+							<label>Фильтр по продукции</label>
+							<select>
+								<option>Вечерние платья</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 <?php get_footer(); ?>
