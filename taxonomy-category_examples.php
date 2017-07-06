278a437e7ac21461feecb47868002c263f3b3797
raidersk8 <romangalchenkov@gmail.com>
2017-07-06 23:30:40 +0300

    update

--- taxonomy-category_examples.php
+++ taxonomy-category_examples.php
@@ -8,7 +8,7 @@
 						<div class="row">
 							<?php $isfirstgroup = true; $i=0; while ( have_posts() ) : the_post(); $i++; ?>
 							<?php if($isfirstgroup == true) : 
-								$propertiesFirst = array(
+								$properties = array(
 									1 => array('open' => '<div class="col-item col-xs-4-5">', 'close'=>'</div>', 'class-item' => 'type-1', 'size'=>'image-273-640'),
 									2 => array('open' => '<div class="col-item col-xs-4-5">', 'close'=>'</div>', 'class-item' => 'type-1', 'size'=>'image-273-640'),
 									3 => array('open' => '<div class="col-item col-xs-3">', 'close'=>'', 'class-item' => 'type-2', 'size'=>'image-179-314'),
@@ -16,33 +16,31 @@
 									5 => array('open' => '<div class="col-item col-xs-3">', 'close'=>'</div>', 'class-item' => 'type-3', 'size'=>'image-179-250'),
 									6 => array('open' => '<div class="col-item col-xs-4-5">', 'close'=>'</div>', 'class-item' => 'type-3', 'size'=>'image-273-250'),
 								);
-							?>
-							<?php echo $propertiesFirst[$i]['open']; ?>
-								<a href="#" class="item <?php echo $propertiesFirst[$i]['class-item']; ?>" style="<?php if($i==1) echo 'margin-top: 260px;'; ?> background-image: url(<?php echo get_the_post_thumbnail_url( get_the_ID(), $propertiesFirst[$i]['size'] ); ?>);">
-								</a>
-							<?php echo $propertiesFirst[$i]['close']; ?>							
-							<?php if($i == 6) { $isfirstgroup = false; $i=0; } ?>
+								viewExample($properties[$i], ($i==1) ? 'margin-top: 260px;' : '');
+							?>					
+							
+							<?php if($i == 6) : $isfirstgroup = false; $i=0; ?></div><div class="row"><?php endif; ?>
 							<?php else : ?>
+								<?php if($i==1) : ?>
+									</div>
+									<div class="row">
+								<?php endif; ?>
+								<?php 
+									$properties = array(
+										1 => array('open' => '<div class="col-item col-xs-3">', 'close'=>'', 'class-item' => 'type-3', 'size'=>'image-179-250'),
+										2 => array('open' => '', 'close'=>'</div>', 'class-item' => 'type-3', 'size'=>'image-179-250'),
+										3 => array('open' => '<div class="col-item col-xs-6">', 'close'=>'</div>', 'class-item' => 'type-4', 'size'=>'image-368-510'),
+										4 => array('open' => '<div class="col-item col-xs-3">', 'close'=>'', 'class-item' => 'type-3', 'size'=>'image-179-250'),
+										5 => array('open' => '', 'close'=>'</div>', 'class-item' => 'type-3', 'size'=>'image-179-250'),
+									);
+									viewExample($properties[$i], '');
+								?>
+								<?php if($i==5) : $i=0; ?>
+									</div>
+									<div class="row">
+								<?php endif; ?>
 							<?php endif; ?>
 							<?php endwhile; ?>
-						</div>
-						<div class="row">
-							<div class="col-item col-xs-3">
-								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/7.png);">
-								</a>		
-								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/8.png);">
-								</a>							
-							</div>
-							<div class="col-item col-xs-6">
-								<a href="#" class="item type-4" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/9.png);">
-								</a>							
-							</div>
-							<div class="col-item col-xs-3">
-								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/10.png);">
-								</a>		
-								<a href="#" class="item type-3" style="background-image: url(<?php echo get_bloginfo('template_url'); ?>/img/examples/100.png);">
-								</a>						
-							</div>
 						</div>
 					</div>
 					<?php endif; ?>
@@ -69,4 +67,11 @@
 			</div>
 		</div>
 	</div>
+<?php
+function viewExample($properte, $style = '') { ?>
+	<?php echo $properte['open']; ?>
+		<a href="#" class="item <?php echo $properte['class-item']; ?>" style="<?php echo $style; ?> background-image: url(<?php echo get_the_post_thumbnail_url( get_the_ID(), $properte['size'] ); ?>);">
+		</a>
+	<?php echo $properte['close']; ?>	
+<?}?>
 <?php get_footer(); ?>
